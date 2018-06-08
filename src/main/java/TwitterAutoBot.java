5fbd0769f0b0894ecc7eb8ff1a09764c25387f0d
Alex Bean <alexfraga10@gmail.com>
2018-06-08 23:16:30 +0100

    Check if the day of the match is the same day as today before tweet it

--- src/main/java/TwitterAutoBot.java
+++ src/main/java/TwitterAutoBot.java
@@ -5,6 +5,7 @@ import twitter4j.TwitterFactory;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class TwitterAutoBot {
@@ -23,14 +24,16 @@ public class TwitterAutoBot {
     private static void sendAllTweets() {
         String line;
         for(int i=0;i<listOfMatches.size();i++) {
-            sendTweet(listOfMatches.get(i).tweetMatch());
-            try {
-                System.out.println("Sleeping for 30 minutes...");
-                //Thread.sleep(1800000); // every 30 minutes
-                Thread.sleep(180000); // every 3 minutes
-                // Thread.sleep(10000); // every 10 seconds
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if(isToday(listOfMatches.get(i))) {
+                sendTweet(listOfMatches.get(i).tweetMatch());
+                try {
+                    System.out.println("Sleeping for 30 minutes...");
+                    //Thread.sleep(1800000); // every 30 minutes
+                    Thread.sleep(180000); // every 3 minutes
+                    // Thread.sleep(10000); // every 10 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
@@ -40,6 +43,8 @@ public class TwitterAutoBot {
 
     }
 
+
+
     private static void sendTweet(String line) {
         Twitter twitter = TwitterFactory.getSingleton();
         Status status;
@@ -48,6 +53,35 @@ public class TwitterAutoBot {
             System.out.println(status);
         } catch (TwitterException e) {;
             e.printStackTrace();
+        }
+    }
+
+    private static boolean isToday(Match match){
+        boolean isToday = false;
+
+        LocalDateTime now = LocalDateTime.now();
+
+        String monthToday = convertToMonth(now.getMonthValue());
+        String dayToday = String.valueOf(now.getDayOfMonth());
+        String monthMatch = match.getDateMatch().split("/")[0];
+        String dayMatch = match.getDateMatch().split("/")[1];
+
+        if(dayToday.equals(dayMatch) && monthToday.equals(monthMatch)){
+            isToday = true;
+        }
+        return isToday;
+    }
+
+    private static String convertToMonth(int month){
+        if(month == 6){
+            return "Jun";
+        }
+        else if(month == 7){
+            return "Jul";
+        }
+        else {
+            //Never happens in out example
+            return "Jul";
         }
     }
 }
